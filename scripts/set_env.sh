735ed5ba67cdc0ba39834234ca6b0cbc3f00eadb
koojun99 <koojun99@naver.com>
2025-03-25 21:57:29 +0900

    👷 Ci: latest

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -2,4 +2,4 @@
 echo "Exporting environment variables..."
 export AWS_ACCOUNT_ID=418295722497
 export AWS_REGION=ap-northeast-2
-export IMAGE_TAG=$(cat /home/ec2-user/app/version.txt)  # 또는 적절한 방법으로 IMAGE_TAG를 설정
+export IMAGE_TAG=latest  # 또는 적절한 방법으로 IMAGE_TAG를 설정
