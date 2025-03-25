520af19f700255bfe6d46d830e78050ca55b67e8
koojun99 <koojun99@naver.com>
2025-03-25 21:24:17 +0900

    👷 Ci: AWS 로그인 추가

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -3,12 +3,15 @@
 # 로그 출력 설정
 exec > >(tee /var/log/restart.sh.log) 2>&1
 
+yum update
+yum install awscli -y
+
 echo "Stopping any existing container named spring-app..."
 docker stop spring-app || true
 docker rm spring-app || true
 
 echo "Logging in to Amazon ECR..."
-$(aws ecr get-login --no-include-email --region ${AWS_REGION})
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/spring-app
 
 echo "Pulling latest image from ECR..."
 docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/spring-app:${IMAGE_TAG}
