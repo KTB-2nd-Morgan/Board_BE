98dc097016a198d57d82bd049576d46f251083f2
koojun99 <koojun99@naver.com>
2025-03-25 19:52:09 +0900

    👷 Ci: 쉘스크립트 수정

--- src/restart.sh
+++ src/restart.sh
@@ -7,10 +7,6 @@ echo "Stopping any existing container named spring-app..."
 docker stop spring-app || true
 docker rm spring-app || true
 
-echo "Logging in to ECR..."
-# AWS CLI가 이미 인스턴스에서 인증되어 있거나, IAM 역할을 통해 인증되는 경우 아래 명령어가 필요하지 않을 수 있습니다.
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
-
 echo "Pulling latest image from ECR..."
 docker pull ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/spring-app:${IMAGE_TAG}
 
