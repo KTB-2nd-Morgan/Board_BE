db85033d8037d196cdb6ae455864ff8490461071
koojun99 <koojun99@naver.com>
2025-07-08 21:06:31 +0900

    ♻️ Refactor: CommonResponse

--- src/main/java/com/example/board/global/response/CommonResponse.java
+++ src/main/java/com/example/board/global/response/CommonResponse.java
@@ -8,42 +8,26 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "data"})
-public record CommonResponse<T> {
-
-
+public record CommonResponse<T>(
+        @JsonProperty("isSuccess") Boolean isSuccess,
+        @JsonProperty("code") String code,
+        @JsonProperty("message") String message,
+        @JsonProperty("data") T data
+) {
 
     @Override
     public String toString() {
         try {
             ObjectMapper mapper = new ObjectMapper();
-            // Java 8 날짜/시간 모듈 등록
             mapper.registerModule(new JavaTimeModule());
-            // 날짜와 시간을 ISO-8601 형식의 문자열로 직렬화
             mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            // 이쁘게 출력하기
             return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
-
-    @JsonProperty("isSuccess")
-    private final Boolean isSuccess;
-
-    @JsonProperty("code")
-    private final String code;
-
-    @JsonProperty("message")
-    private final String message;
-
-    @JsonProperty("data")
-    private T data;
 
     public static <T> CommonResponse<T> onSuccess(T data) {
         return new CommonResponse<>(true, "200", SuccessStatus.SUCCESS.getMessage(), data);
@@ -56,12 +40,11 @@ public record CommonResponse<T> {
     public static <T> CommonResponse<T> onNoContent() {
         return new CommonResponse<>(
                 true,
-                SuccessStatus.NO_CONTENT.getCode(), // 204 코드 가져오기
-                SuccessStatus.NO_CONTENT.getMessage(), // "콘텐츠 없음" 메시지 가져오기
+                SuccessStatus.NO_CONTENT.getCode(),
+                SuccessStatus.NO_CONTENT.getMessage(),
                 null
         );
     }
-
 
     public static <T> CommonResponse<T> onFailure(String code, String message, T data) {
         return new CommonResponse<>(false, code, message, data);
@@ -70,5 +53,4 @@ public record CommonResponse<T> {
     public static <T> CommonResponse<T> onFailure(BaseCode code, T data) {
         return new CommonResponse<>(false, code.getReasonHttpStatus().getCode(), code.getReasonHttpStatus().getMessage(), data);
     }
-
 }
